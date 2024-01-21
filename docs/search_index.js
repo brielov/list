b7ac6ce9db9670e3e4140ba98e82c2aa41837e7c
Gabriel Vaquer <brielov@icloud.com>
2024-01-20 23:38:09 -0300

    Added docs to gitignore

--- docs/search_index.js
+++
@@ -1,3 +0,0 @@
-(function () {
-  window.DENO_DOC_SEARCH_INDEX = {"nodes":[{"kind":"class","name":"List","file":".","nsQualifiers":"","location":{"filename":".","line":65,"col":0},"declarationKind":"export"}]};
-})()
